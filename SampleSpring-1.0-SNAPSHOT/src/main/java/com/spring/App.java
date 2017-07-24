83d8f784c5bcb65dcecf781f01a4f0d07dd9ead3
kyawmyintthu <kyawmyiintthu@gmail.com>
2017-07-24 16:31:35 +0800

    added test

--- SampleSpring-1.0-SNAPSHOT/src/main/java/com/spring/App.java
+++ SampleSpring-1.0-SNAPSHOT/src/main/java/com/spring/App.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 /**
  * Hello world!
- *
+ * Edited in branch...
  */
 public class App {
 	public static void main(String[] args) {
